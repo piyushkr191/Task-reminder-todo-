d16a623dc59f8ef349915344210292fa51b1476b
PIYUSH KUMAR <120174672+piyushkr191@users.noreply.github.com>
2024-11-16 07:17:13 +0530

    Update account.js

--- src/emails/account.js
+++ src/emails/account.js
@@ -7,7 +7,7 @@ sgMail.setApiKey(process.env.SG_API_KEY);
 const sendWelcomeMail = (email,name)=>{
     sgMail.send({
         to:email,
-        from:'paritoshdev26@gmail.com',
+        from:'piyushkr.seek@gmail.com',
         subject:'Thanks for Joining In!',
         text:`Hi, ${name}, welcome to the App. Now never forget Anything. Don't forget to complete your user profile!!`
     })
@@ -16,7 +16,7 @@ const sendWelcomeMail = (email,name)=>{
 const sendGoodbyeMail = (email,name)=>{
     sgMail.send({
         to:email,
-        from:'paritoshdev26@gmail.com',
+        from:'piyushkr.seek@gmail.com',
         subject:'Sorry to see you go',
         text:`Goodbye, ${name}. Hope to see you back soon sometime in near future.`
     })
